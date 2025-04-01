37b33e80067f8bfae7955f7f1911e19c1284e3c9
Derek Reynolds <superDreynolds@gmail.com>
2025-03-31 23:59:25 -0500

    MSSQL Connection Proof of Concept

--- Ups_Downs_API/Ups_Downs_API.Web/WeatherApiClient.cs
+++ Ups_Downs_API/Ups_Downs_API.Web/WeatherApiClient.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.Data.SqlClient;
+
 namespace Ups_Downs_API.Web;
 
 public class WeatherApiClient(HttpClient httpClient)
@@ -20,6 +23,84 @@ public class WeatherApiClient(HttpClient httpClient)
         }
 
         return forecasts?.ToArray() ?? [];
+    }
+
+
+    public string ConnectToDB()
+    {
+        string names = "";
+        try
+        {
+            // Build connection string
+            Microsoft.Data.SqlClient.SqlConnectionStringBuilder builder =
+                new SqlConnectionStringBuilder("Server=.\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            // Connect to SQL
+            Console.Write("Connecting to SQL Server ... ");
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Done.");
+
+                // READ demo
+                Console.WriteLine("Reading from DB...");
+                string sql = "SELECT FirstName, LastName FROM TEST_Gamers;";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            names += reader.GetString(0);
+                            names += " ";
+                            names += reader.GetString(1);
+                            names += ", ";
+                        }
+                        Console.WriteLine("Done.");
+                    }
+                }
+            }
+        }
+        catch (SqlException e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        return names;
+    }
+
+    public void AddDBEntry()
+    {
+        try
+        {
+            // Build connection string
+            Microsoft.Data.SqlClient.SqlConnectionStringBuilder builder =
+                new SqlConnectionStringBuilder("Server=.\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            // Connect to SQL
+            Console.Write("Connecting to SQL Server ... ");
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Done.");
+
+                // Insert some sample data
+                Console.WriteLine("Inserting new data...");
+                StringBuilder sb = new StringBuilder();
+                sb.Append("USE master; ");
+                sb.Append("INSERT INTO TEST_Gamers (FirstName, LastName) VALUES ");
+                sb.Append("('Another', 'Dude!');");
+                string sql = sb.ToString();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Done.");
+                }
+            }
+        }
+        catch (SqlException e)
+        {
+            Console.WriteLine(e.ToString());
+        }
     }
 }
 
