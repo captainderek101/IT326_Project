9908719e57e69a525f3bdbcf8fc1e39d798bc4e8
DaneIwema <daneiwema@gmail.com>
2025-04-10 21:14:26 -0500

    made significant progress
    created the service and controller files for the login page, also created the login page, and created the UserObject class

---
+++ Ups_Downs_API/Ups_Downs_API.ApiService/Controllers/LoginPageController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Library;
+using Ups_Downs_API.ApiService.Services;
+
+namespace Ups_Downs_API.ApiService.Controllers
+{
+    [ApiController]
+    [Route("login")]
+    public class LoginPageController : ControllerBase
+    {
+        private readonly LoginService _loginService;
+
+        public LoginPageController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult recieveLoginRequest([FromBody] UserObject receivedObject)
+        {
+            //logic for service file connection
+            //validating the model, if model is not valid send a BadRequest
+            if (receivedObject.validateLogin())
+            {
+                return BadRequest("Missing Requirments");
+            }
+
+            //service logic
+            _loginService.ProcessLoginPost(receivedObject);
+
+            return Ok();
+        }
+
+        [HttpGet("create")]
+        public IActionResult recieveAccountCreationRequest([FromBody] UserObject receivedObject)
+        {
+            //logic for service file connection
+            //validating the model, if model is not valid send a BadRequest
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //service logic
+            _loginService.ProcessAccountCreationPost(receivedObject);
+
+            return Created();
+        }
+
+        [HttpGet("forgotpw")]
+        public IActionResult recieveForgotPwRequest([FromBody] UserObject receivedObject)
+        {
+            //logic for service file connection
+            //validating the model, if model is not valid send a BadRequest
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //service logic
+            _loginService.ProcessForgotPwPost(receivedObject);
+
+            return Ok();
+        }
+    }
+}
---
+++ Ups_Downs_API/Ups_Downs_API.ApiService/Services/LoginService.cs
@@ -0,0 +1,37 @@
+﻿using Library;
+
+namespace Ups_Downs_API.ApiService.Services
+{
+    public class LoginService
+    {
+        public object ProcessLoginGet(UserObject obj)
+        {
+            // TODO: DB connection here
+            //obj.UserName = "success";
+            //obj.Password = "double success";
+
+
+            //var v = new
+            //{
+            //    Name = obj.Name,
+            //    Password = obj.Password
+            //};
+
+            return obj;
+        }
+        public void ProcessLoginPost(UserObject obj)
+        {
+            // TODO: DB connection here
+        }
+
+        public void ProcessAccountCreationPost(UserObject obj)
+        {
+            // TODO: DB connection here
+        }
+
+        public void ProcessForgotPwPost(UserObject obj)
+        {
+            // TODO: DB connection here
+        }
+    }
+}
