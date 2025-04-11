1f1a1c91de365aa57c236f2385a53c22462841a3
DaneIwema <daneiwema@gmail.com>
2025-04-10 21:14:26 -0500

    made significant progress
    created the service and controller files for the login page, also created the login page, and created the UserObject class

---
+++ Ups_Downs_API/Library/UserObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class UserObject
+    {
+        private string? UserName { get; set; }
+
+        private int? UserId { get; set; }
+        private string? PassWord { get; set; }
+
+        public UserObject(string name, string pw)
+        {
+            UserName = name;
+            PassWord = pw;
+        }
+
+        public bool validateLogin() //returns true if there is a username and password saved
+        {
+            if (string.IsNullOrWhiteSpace(UserName) || string.IsNullOrWhiteSpace(PassWord))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
