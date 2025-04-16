e64608b1ff02578a6aa45a3bc4d5d7f10e5fbf35
JTP02 <134463999+JTP02@users.noreply.github.com>
2025-04-15 22:45:29 -0500

    Creating post back end logic

---
+++ Ups_Downs_API/Library/CreatingPostObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Library
+{
+    public class CreatingPostObject
+    {
+        public required int PostId { get; set; }
+        public required string PostDayType { get; set; }
+        public required string PostTimestamp { get; set; }
+        public int UserId { get; set; }
+        public required string Content { get; set; }
+    }
+}
