58715e29b6424acddccee73467351c4abd218f49
Derek Reynolds <superDreynolds@gmail.com>
2025-04-16 23:54:30 -0500

    move DB connection from controller to service

--- Ups_Downs_API/Ups_Downs_API.ApiService/Controllers/ViewPostController.cs
+++ Ups_Downs_API/Ups_Downs_API.ApiService/Controllers/ViewPostController.cs
@@ -14,13 +14,11 @@ namespace Ups_Downs_API.ApiService.Controllers
     public class ViewPostController : ControllerBase
     {
         private readonly ViewPostService _viewPostService;
-        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
 
         // Constructor Injection
-        public ViewPostController(ViewPostService viewPostService, IDbContextFactory<ApplicationDbContext> contextFactory)
+        public ViewPostController(ViewPostService viewPostService)
         {
             _viewPostService = viewPostService;
-            _contextFactory = contextFactory;
         }
 
         [HttpGet(Name = "view")] //if page has different types of GET or POST requests we need to diversify the call like this
@@ -33,23 +31,6 @@ namespace Ups_Downs_API.ApiService.Controllers
             {
                 return BadRequest(ModelState);
             }
-            string names = "";
-            using (var context = _contextFactory.CreateDbContext())
-            {
-                var connection = (SqlConnection)context.Database.GetDbConnection();
-                var command = new SqlCommand("SELECT FirstName, LastName FROM TEST_Gamers;", connection);
-                connection.Open();
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    names += reader.GetString(0);
-                    names += " ";
-                    names += reader.GetString(1);
-                    names += ", ";
-                }
-                reader.Close();
-            }
-            Console.WriteLine(names);
 
             //service logic - sends object to method in service.
             var test = _viewPostService.GetPost(receivedObject);
--- Ups_Downs_API/Ups_Downs_API.ApiService/Services/ViewPostService.cs
+++ Ups_Downs_API/Ups_Downs_API.ApiService/Services/ViewPostService.cs
@@ -1,14 +1,42 @@
 ﻿using Library;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Ups_Downs_API.ApiService.Database;
 
 namespace Ups_Downs_API.ApiService.Services
 {
     public class ViewPostService
     {
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+
+        // Constructor Injection
+        public ViewPostService(IDbContextFactory<ApplicationDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
         public object GetPost(PostObject obj)
         {
             // TODO: DB connection here
             obj.Content = "test";
 
+            string names = "";
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                var connection = (SqlConnection)context.Database.GetDbConnection();
+                var command = new SqlCommand("SELECT FirstName, LastName FROM TEST_Gamers;", connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    names += reader.GetString(0);
+                    names += " ";
+                    names += reader.GetString(1);
+                    names += ", ";
+                }
+                reader.Close();
+            }
+            Console.WriteLine(names);
 
             var v = new
             {
