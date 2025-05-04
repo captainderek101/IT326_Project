452316e357c4cf15b1405059b8625951c5dc3ff2
Drake Montgomery <dmontg1@ilstu.edu>
2025-05-04 03:21:26 -0500

    Db integration, page complete

--- Ups_Downs_API/Ups_Downs_API.ApiService/Services/HomePageService.cs
+++ Ups_Downs_API/Ups_Downs_API.ApiService/Services/HomePageService.cs
@@ -9,6 +9,7 @@ using worldnewsapi.Api;
 using worldnewsapi.Client;
 using worldnewsapi.Model;
 using static System.Net.WebRequestMethods;
+using System.Runtime.CompilerServices;
 
 namespace Ups_Downs_API.ApiService.Services
 {
@@ -26,50 +27,54 @@ namespace Ups_Downs_API.ApiService.Services
         public CurrentEvents getCurrentEvents()
         {
             CurrentEvents currentEvents = new CurrentEvents();
-            
-            //bool isOutdated = true; // Check if db info outdated
 
-            // Db connection
-            //using (var context = _contextFactory.CreateDbContext())
-            //{
-                //var connection = (SqlConnection)context.Database.GetDbConnection();
-                //string query = "SELECT TOP 1 time FROM Articles";
-                //var command = new SqlCommand(query, connection);
+            bool isOutdated = true; // Check if db info outdated
+            string today = DateTime.Today.ToString("yyyy-MM-dd");
+            //  Db connection
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                var connection = (SqlConnection)context.Database.GetDbConnection();
+                string query = "SELECT TOP 1 time FROM Articles;";
+                var command = new SqlCommand(query, connection);
 
-                //connection.Open();
-                //using (var reader = command.ExecuteReader())
-                //{
-                    // 3 cases - db is empty, db is outdated, or db is up to date
-                    // Checking timestamp/if database is outdated
-                  //  while (reader.Read())
-                    //{
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    //3 cases - db is empty, db is outdated, or db is up to date
+                    //Checking timestamp/if database is outdated
+                    while (reader.Read())
+                    {
                         // If the date lines up, then they aren't outdated
-                      //  if ((reader.GetString(0) == DateTime.Today.ToString("YYYY-MM-dd")))
-                        //{
-                           // isOutdated = false;
-                        //}
-                    //}
-                //}
-                //if (!isOutdated)
-                //{
-                    // If not outdated, then read articles into current events
-                  //  query = "SELECT title, url FROM Articles";
-                    //command = new SqlCommand(query, connection);
+                        isOutdated = (reader.GetString(0) != today);
+                    }
+                }
+                if (!isOutdated)
+                {
+                    //  If not outdated, then read articles into current events
+                    query = "SELECT title, url FROM Articles;";
+                    command = new SqlCommand(query, connection);
 
-                    //using (var reader = command.ExecuteReader()) { 
-                    // reading articles into db
-                    //while (reader.Read())
-                    //{
-                    //    currentEvents.articles.Add(new NewsArticle(reader.GetString(0), reader.GetString(1)));
-                    //}
-                //}
+                    using (var reader = command.ExecuteReader())
+                    {
+                        //  reading articles into db
+                        while (reader.Read())
+                        {
+                            currentEvents.articles.Add(new NewsArticle(reader.GetString(0), reader.GetString(1)));
+                        }
+                    }
 
                     // If articles outdated, read new articles
-                //}
-                //else
-                //{
+                }
+                else
+                {
+                    //clearing database of outdated entries
+                    query = "DELETE FROM Articles;";
+                    command = new SqlCommand(query, connection);
+
+                    command.ExecuteNonQuery();
+
                     // Insert world news api key here!
-                    string apiKey = "";
+                    string apiKey = "c9d94381de234cb595e7ca4dcb920320";
                     // Copied shamelessly from the exapmle given
                     Configuration config = new Configuration();
                     config.BasePath = "https://api.worldnewsapi.com";
@@ -89,7 +94,7 @@ namespace Ups_Downs_API.ApiService.Services
                     string language = "en";
                     double minSentiment = 0.7;
                     double maxSentiment = 1;
-                    string earliestPublishDate = DateTime.Today.AddDays(-7).ToString("yyyy-MM-dd"); 
+                    string earliestPublishDate = DateTime.Today.AddDays(-7).ToString("yyyy-MM-dd");
                     string? latestPublishDate = null;
                     string? newsSources = null;
                     string? authors = null;
@@ -105,19 +110,20 @@ namespace Ups_Downs_API.ApiService.Services
                     {
                         // Search News
                         SearchNews200Response result = apiInstance.SearchNews(text, textMatchIndexes, sourceCountry, language, minSentiment, maxSentiment, earliestPublishDate, latestPublishDate, newsSources, authors, categories, entities, locationFilter, sort, sortDirection, offset, number);
-                // Put News Results into current events
-                currentEvents.positiveMessage = "kill yourself";
+                        // Put News Results into current events
                         for (int i = 0; i < 3; i++)
                         {
-                            
-                            currentEvents.articles.Add(new NewsArticle(result.News[i].Title, result.News[i].Url));
-                    Console.WriteLine(currentEvents.articles[i].DisplayArticle());
-                            //query = "INSERT INTO Articles VALUES(" + result.News[i].Title + ", " + result.News[i].Url + ", " + DateTime.Today.ToString("yyyy-MM-dd") + ")";
-                            //command = new SqlCommand(query, connection);
 
-                            //using var reader = command.ExecuteReader();
+                            currentEvents.articles.Add(new NewsArticle(result.News[i].Title, result.News[i].Url));
+
+                            command = new SqlCommand("INSERT INTO Articles VALUES (@Title, @Url, @time);", connection);
+                            command.Parameters.AddWithValue("@Title", result.News[i].Title);
+                            command.Parameters.AddWithValue("@Url", result.News[i].Url);
+                            command.Parameters.AddWithValue("@time", today);
+
+
+                            command.ExecuteNonQuery();
                         }
-                        return currentEvents;
                     }
                     catch (ApiException e)
                     {
@@ -125,68 +131,90 @@ namespace Ups_Downs_API.ApiService.Services
                         Debug.Print("Status Code: " + e.ErrorCode);
                         Debug.Print(e.StackTrace);
                     }
-                //}
+                }
                 // Get Positive message from quote table
                 // determine if quote chosen for day, if not choose quote
-                //isOutdated = true;
-                //query = "SELECT time FROM CurQuote";
-                //command = new SqlCommand(query, connection);
+                isOutdated = true;
+                query = "SELECT time FROM CurQuote;";
+                command = new SqlCommand(query, connection);
 
-                //using (var reader = command.ExecuteReader())
-                //{
+                using (var reader = command.ExecuteReader())
+                {
                     // Checking if date chosen is up to date
-                  //  if (reader.Read())
-                    //{
-                      //  if ((reader.GetString(0) == DateTime.Today.ToString("YYYY-MM-DD")))
-                        //{
-                        //    isOutdated = false;
-                    //    }
-                    //}
-               // }
-               // if (!isOutdated)
-               // {
+                    if (reader.Read())
+                    {
+                        isOutdated = (reader.GetString(0) != today);
+                    }
+                }
+                if (!isOutdated)
+                {
                     // Finding quote from CurQuote Table
-                 //   query = "SELECT index FROM CurQuote";
-                  //  command = new SqlCommand(query, connection);
-                   // string index;
-                   // using (var reader = command.ExecuteReader()){
+                    query = "SELECT id FROM CurQuote;";
+                    command = new SqlCommand(query, connection);
+                    int index;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            index = reader.GetInt32(0);
+                        }
+                        else
+                        {
+                            currentEvents.positiveMessage = "Error";
+                            return currentEvents;
+                        }
+                    }
+                    query = "SELECT text FROM Quotes WHERE id = " + index + ";";
+                    command = new SqlCommand(query, connection);
 
-                     //   index = reader.GetString(0);
-                    //}
-                   // query = "SELECT text FROM Quotes WHERE id = " + index;
-                   // command = new SqlCommand(query, connection);
-
-                   // using (var reader = command.ExecuteReader()){
-
-                   //     currentEvents.positiveMessage = reader.GetString(0);
-                   // }
-                //}
-                //else
-                //{
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            currentEvents.positiveMessage = reader.GetString(0);
+                        }
+                        else
+                        {
+                            currentEvents.positiveMessage = "Error";
+                            return currentEvents;
+                        }
+                    }
+                }
+                else
+                {
                     // Select new quote
-                  //  query = "DELETE FROM CurQuote";
-                  //  command = new SqlCommand(query, connection);
+                    query = "DELETE FROM CurQuote;";
+                    command = new SqlCommand(query, connection);
 
-                  //  command.ExecuteNonQuery();
+                    command.ExecuteNonQuery();
 
                     // find random entry in SQL
-                    //query = "SELECT TOP 1 id, text FROM Quotes ORDER BY NEWID()";
-                    //command = new SqlCommand(query, connection);
-                    //int index;
-                    //using (var reader = command.ExecuteReader())
-                    //{
-                    //    index = reader.GetInt16(0);
-                    //    currentEvents.positiveMessage = reader.GetString(0);
-                    //}
+                    query = "SELECT TOP 1 id, text FROM Quotes ORDER BY NEWID();";
+                    command = new SqlCommand(query, connection);
+                    int index;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            index = reader.GetInt32(0);
+                            currentEvents.positiveMessage = reader.GetString(1);
+                        }
+                        else
+                        {
+                            currentEvents.positiveMessage = "Error";
+                            return currentEvents;
+                        }
+                    }
                     // Save result to table
-                   // query = "INSERT INTO CurQuote VALUES(" + index + ", " + DateTime.Today.ToString("yyyy-MM-dd") + ")";
-                   // command = new SqlCommand(query, connection);
+                    command = new SqlCommand("INSERT INTO CurQuote VALUES (@id, @time);", connection);
+                    command.Parameters.AddWithValue("@id", index);
+                    command.Parameters.AddWithValue("@time", today);
 
-                   // command.ExecuteNonQuery();
-                   // connection.Close();
-                //}
-                return currentEvents;
-            //}
+                    command.ExecuteNonQuery();
+                    }
+                connection.Close();
+            }
+            return currentEvents;
         }
     }
 }
